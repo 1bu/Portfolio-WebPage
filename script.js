4f97a279833b3b43062d2e7eea672edce347ddaf
Iván Carpio <67491616+1bu@users.noreply.github.com>
2024-03-30 18:46:01 -0400

    Responsive and styles updates

--- script.js
+++ script.js
@@ -2,10 +2,31 @@ const open = document.getElementById('open');
 const close = document.getElementById('close');
 const modal = document.getElementById('modal-container');
 
+const mobileNav = document.getElementById("header-items-sm");
+const openBar = document.getElementById("openBar");
+const closeBar = document.getElementById("closeBar");
+
+let currentIndex = 0;
+let cards = [];
+const projectCards = document.querySelectorAll(".project_");
+console.log(projectCards);
+
 open.addEventListener('click',()=>{
-    modal.style.visibility = 'visible'
+    modal.style.visibility = 'visible';
 });
 
 close.addEventListener('click', ()=>{
-    modal.style.visibility = 'hidden'
-})
+    modal.style.visibility = 'hidden';
+});
+
+openBar.addEventListener('click', ()=>{
+    mobileNav.style.visibility = 'visible';    
+    openBar.style.display = 'none';
+    closeBar.style.display = 'inline';
+});
+
+closeBar.addEventListener('click', ()=>{
+    mobileNav.style.visibility = 'hidden';    
+    openBar.style.display = 'inline';
+    closeBar.style.display = "none";
+});
